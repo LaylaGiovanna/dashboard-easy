0b869744bdbac36d8d8e48a28b63f80c0c830905
Layla Giovanna <109964242+LaylaGiovanna@users.noreply.github.com>
2023-12-06 09:51:45 -0300

    first commit

--- pages/WebSocketClient.js
+++ pages/WebSocketClient.js
@@ -1,9 +1,23 @@
-import io from "socket.io-client";
-//const socket = io("http://10.107.144.05:9000", {
-const socket = io("http://localhost:9000", {
-//const socket = io("http://socket-easy4u.azurewebsites.net", {
+// pages/WebSocketClient.js
 
-  transports: ["websocket", "polling"]
-});
+import React, { useEffect } from 'react';
+import io from 'socket.io-client';
 
-export default socket;
+const WebSocketClient = () => {
+  useEffect(() => {
+    const socket = io('http://localhost:9000', {
+      transports: ['websocket', 'polling'],
+    });
+
+    // Adicione lógica para manipular eventos do socket, se necessário
+
+    return () => {
+      // Lógica para limpar recursos quando o componente é desmontado
+      socket.disconnect();
+    };
+  }, []);
+
+  return <div>WebSocketClient Page</div>;
+};
+
+export default WebSocketClient;

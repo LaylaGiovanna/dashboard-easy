1a997e8cadeffd0361c213a3152441ac7a953809
Layla Giovanna <109964242+LaylaGiovanna@users.noreply.github.com>
2023-12-06 09:14:33 -0300

    teste deploy

--- components/LogoutPopup.jsx
+++ components/LogoutPopup.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import classNames from 'classnames';
+// import React from 'react';
+// import classNames from 'classnames';
 
-const LogoutPopup = ({ onClose }) => {
-  return (
-    <div className={classNames('popup')}>
-      <p>Conteúdo do Popup</p>
-      <button onClick={onClose}>Fechar</button>
-    </div>
-  );
-};
+// const LogoutPopup = ({ onClose }) => {
+//   return (
+//     <div className={classNames('popup')}>
+//       <p>Conteúdo do Popup</p>
+//       <button onClick={onClose}>Fechar</button>
+//     </div>
+//   );
+// };
 
-export default LogoutPopup;
+// export default LogoutPopup;
